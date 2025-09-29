e5a95708f3abebde7c593d035beaadc33052abbe
Deepak Sharma <161618299+deepak-sh-07@users.noreply.github.com>
2025-09-29 09:31:48 +0530

    Leetcode-3676

---
+++ Count Bowl Subarrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long bowlSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n,-1);
+        vector<int> right(n,n);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i]) st.pop();
+            if(!st.empty()){
+                left[i] = st.top();
+            }
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<nums[i]) st.pop();
+            if(!st.empty()){
+                right[i] = st.top();
+            }
+            st.push(i);
+        }
+        long long ans = 0;
+        for(int i=0;i<n;i++){
+            if(left[i]!=-1 && right[i]!=n) ans++;
+        }
+        return ans;
+    }
+};
